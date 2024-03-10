0c9c2c3d5b4267b668e25e3386d1326fb60d437e
LAPTOP-LCRCBR4G\glall <g.lallemand@gmail.com>
2024-03-10 22:34:00 +0100

    1st commit

---
+++ src/lib/component/modal.jsx
@@ -0,0 +1,17 @@
+import './modal.css'
+
+export default function Modal({showModal, closeModal, message}) {
+    return (
+        <>
+            {
+                showModal &&
+                <div className="modal" >
+                    <div className="modal-content">
+                        <span className="modal-close-icon" onClick={closeModal}>✖</span>
+                        <span className="message">{message}</span>
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
